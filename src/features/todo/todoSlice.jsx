7b4c9a3755b386f7be9beeacad8e0d43b5cb2f21
unknown <nerwinalamas@gmail.com>
2023-10-08 12:32:51 +0800

    first commit

---
+++ src/features/todo/todoSlice.jsx
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: [
+    {
+      id: 1,
+      desc: "Test 1",
+      completed: true
+    },
+    {
+      id: 2,
+      desc: "Test 2",
+      completed: false
+    },
+    {
+      id: 3,
+      desc: "Test 3",
+      completed: true
+    },
+  ],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.value.push(action.payload);
+    },
+    deleteTodo: (state, action) => {
+      state.value = state.value.filter((t) => t.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, updateTodo, isCompleteTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
